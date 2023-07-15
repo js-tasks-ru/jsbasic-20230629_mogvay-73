c5a25dba2a7846f7b1ce5f9ab1ea407d21d4a670
Mogvay-web <mogvay73@gmail.com>
2023-07-16 01:32:48 +0300

    3.5 task solution

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,18 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ").map((item) => Number(item)),
+    arrNumber = arr.filter((item) => Boolean(item));
+
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+
+  arrNumber.sort(compareNumeric);
+
+  let user = {};
+  user.min = arrNumber[0];
+  user.max = arrNumber.at(-1);
+
+  return user;
 }
