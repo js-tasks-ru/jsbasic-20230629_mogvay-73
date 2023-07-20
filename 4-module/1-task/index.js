98cb34efb4caaf06dbc9d9404a60fdb96c8ebafd
Mogvay-web <mogvay73@gmail.com>
2023-07-20 07:50:35 +0300

    4.1 task solution

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement("ul");
+
+  for (let i = 0; i < friends.length; i++) {
+    ul.innerHTML += `<li>${
+      friends[i].firstName + " " + friends[i].lastName
+    }</li>`;
+  }
+
+  return ul;
 }
