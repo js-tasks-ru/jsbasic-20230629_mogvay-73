331ad2dd4da5e76b0a6a014685199719640d7b52
Mogvay-web <mogvay73@gmail.com>
2023-07-23 11:35:28 +0300

    4.3 task solution

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i],
+      status = row.cells[3],
+      gender = row.cells[2].innerHTML,
+      age = row.cells[1].innerHTML;
+
+    status.hasAttribute("data-available")
+      ? status.dataset.available == "true"
+        ? row.classList.add("available")
+        : row.classList.add("unavailable")
+      : row.setAttribute("hidden", "hidden");
+
+    gender == "m" ? row.classList.add("male") : row.classList.add("female");
+
+    if (age < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
