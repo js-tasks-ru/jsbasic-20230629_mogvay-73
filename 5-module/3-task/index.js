17cc387bb9f0d00d77f9dccab707689a123bbd2d
Mogvay-web <mogvay73@gmail.com>
2023-07-30 00:35:43 +0300

    5.3 task solution

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const inner = document.querySelector(".carousel__inner"),
+    arrowLeft = document.querySelector(".carousel__arrow_left"),
+    arrowRight = document.querySelector(".carousel__arrow_right");
+
+  let shift = 0,
+    i = 0;
+  arrowLeft.style.display = "none";
+
+  arrowLeft.addEventListener("click", function () {
+    shift += inner.offsetWidth;
+    arrowRight.style.display = "";
+    i--;
+
+    if (i == 0) {
+      this.style.display = "none";
+    }
+
+    inner.style.transform = `translateX(${shift}px)`;
+  });
+
+  arrowRight.addEventListener("click", function () {
+    shift -= inner.offsetWidth;
+    arrowLeft.style.display = "";
+    i++;
+
+    if (i == 3) {
+      this.style.display = "none";
+    }
+
+    inner.style.transform = `translateX(${shift}px)`;
+  });
 }
